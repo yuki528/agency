bf1760b5475b187d80456bad2d0bf826ead76e18
yuki528 <kiplimomeli44@gmail.com>
2022-02-07 22:48:31 +0300

    Agency

---
+++ .cache/async-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-thanks-js": () => import("./../../../src/pages/thanks.js" /* webpackChunkName: "component---src-pages-thanks-js" */),
+  "component---src-pages-works-js": () => import("./../../../src/pages/works.js" /* webpackChunkName: "component---src-pages-works-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,14 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Ian kiplimo\\gatsby\\agency\\.cache\\dev-404-page.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\Ian kiplimo\\gatsby\\agency\\src\\pages\\about.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Ian kiplimo\\gatsby\\agency\\src\\pages\\index.js"))),
+  "component---src-pages-thanks-js": hot(preferDefault(require("C:\\Users\\Ian kiplimo\\gatsby\\agency\\src\\pages\\thanks.js"))),
+  "component---src-pages-works-js": hot(preferDefault(require("C:\\Users\\Ian kiplimo\\gatsby\\agency\\src\\pages\\works.js")))
+}
+
